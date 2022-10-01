489683e07f8b1baaa2a52ae598407331075ef87d
kardibus <kardibus@gmail.com>
2022-10-01 21:30:36 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Button variant={"primary"}>push</Button>
+        </div>
+    )
+        ;
+}
+
+export default App;
